963696f78a14c143d3f53a1b00ef66a8104856e8
QuangPhung15 <>
2022-11-02 02:35:04 +0000

    L6 - Checking that the player's move is valid

--- main.py
+++ main.py
@@ -12,19 +12,23 @@ boxVals = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 # Print the starting, empty board.
 prBrd(boxVals[0], boxVals[1], boxVals[2], boxVals[3], boxVals[4], boxVals[5], boxVals[6], boxVals[7], boxVals[8])
+validMove = [boxVals[0], boxVals[1], boxVals[2], boxVals[3], boxVals[4], boxVals[5], boxVals[6], boxVals[7], boxVals[8]]
 
 # Start a loop that will repeat for more times than the game will need.
 for i in range(20):
 	# PLAYER TURN
 	# Get the user's move and assert that it's on the board.
 	uMove = input("Please choose the box that you want to go with \n>> ")
+	while not uMove in validMove:
+		uMove = input("Please choose the box that you want to go with \n>> ")
 	# Change the chosen box's value to "x" if it was the blank before the user's move.
 	boxVals[int(uMove) - 1] = "x"
+	validMove.remove(uMove)
 	# Print the board to show the user's move.
 	prBrd(boxVals[0], boxVals[1], boxVals[2], boxVals[3], boxVals[4], boxVals[5], boxVals[6], boxVals[7], boxVals[8])
 	# Create a variable to hold the return value of checkWin().
 	# If the user has won, congratulate the player and exit the program.
-
+	
 	# COMPUTER TURN
 	# Generate a random move by the computer.
 	cMove = str(randint(1, 9))
