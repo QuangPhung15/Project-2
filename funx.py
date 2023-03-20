6229e1df6a35996908c94e1cb53f3f724eef06b9
QuangPhung15 <106754129+QuangPhung15@users.noreply.github.com>
2023-03-20 13:53:50 +0000

    GitHub commit

--- funx.py
+++ funx.py
@@ -1,20 +1,125 @@
 from random import *
+from graphics import *
 
 # define fuctions
-def prRow(box1, box2, box3):
-	"""prints a particular row of 3 boxes"""
-	print(" " + box1 + " | " + box2 + " | " + box3)
-
-def prBrd(box11, box12, box13, box21, box22, box23, box31, box32, box33):
+def prBrd(width, height, win):
 	"""prints the entire board"""
-	divider = "---+---+---"
-	# call prRow() function and print divider between to draw the board
-	prRow(box11, box12, box13)
-	print(divider)
-	prRow(box21, box22, box23)
-	print(divider)
-	prRow(box31, box32, box33)
+	for i in range(3):
+		for j in range(3):
+			box = Rectangle(Point(0 + width * j, 0 + height * i), Point(0 + width * (j + 1), 0 + height * (i + 1)))
+			box.setFill("white")
+			box.draw(win)
 
+def drawX(xX, xY, xWin):
+	"""draws X based on the center of the box"""
+	x1 = Polygon(Point(xX - 25, xY - 25), Point(xX - 20, xY - 30), Point(xX + 25, xY + 25), Point(xX + 20, xY + 30))
+	x2 = Polygon(Point(xX + 25, xY - 25), Point(xX + 20, xY - 30), Point(xX - 25, xY + 25), Point(xX - 20, xY + 30))
+	x1.setFill("blue")
+	x1.setOutline("blue")
+	x2.setFill("blue")
+	x2.setOutline("blue")
+	x1.draw(xWin)
+	x2.draw(xWin)
+
+def drawO(oX, oY, oWin):
+	"""draws O based on the center of the box"""
+	o1 = Circle(Point(oX, oY), 30)
+	o2 = Circle(Point(oX, oY), 24)
+	o1.setFill("red")
+	o1.setOutline("red")
+	o2.setFill("white")
+	o2.setOutline("red")
+	o1.draw(oWin)
+	o2.draw(oWin)
+
+def drawLine(box11, box12, box13, box21, box22, box23, box31, box32, box33, lWin):
+	"""draw a line showing where the winning move is"""
+	if box11 == box12 == box13:
+		line = Line(Point(10, 250), Point(290, 250))
+		line.setWidth(8)
+		line.setOutline(color_rgb(84, 255, 0))
+		line.draw(lWin)
+	elif box21 == box22 == box23:
+		line = Line(Point(10, 150), Point(290, 150))
+		line.setWidth(8)
+		line.setOutline(color_rgb(84, 255, 0))
+		line.draw(lWin)
+	elif box31 == box32 == box33:
+		line = Line(Point(10, 50), Point(290, 50))
+		line.setWidth(8)
+		line.setOutline(color_rgb(84, 255, 0))
+		line.draw(lWin)
+	elif box11 == box21 == box31:
+		line = Line(Point(50, 290), Point(50, 10))
+		line.setWidth(8)
+		line.setOutline(color_rgb(84, 255, 0))
+		line.draw(lWin)
+	elif box12 == box22 == box32:
+		line = Line(Point(150, 290), Point(150, 10))
+		line.setWidth(8)
+		line.setOutline(color_rgb(84, 255, 0))
+		line.draw(lWin)
+	elif box13 == box23 == box33:
+		line = Line(Point(250, 290), Point(250, 10))
+		line.setWidth(8)
+		line.setOutline(color_rgb(84, 255, 0))
+		line.draw(lWin)
+	elif box11 == box22 == box33:
+		line = Line(Point(15, 285), Point(285, 15))
+		line.setWidth(8)
+		line.setOutline(color_rgb(84, 255, 0))
+		line.draw(lWin)
+	elif box13 == box22 == box31:
+		line = Line(Point(285, 285), Point(15, 15))
+		line.setWidth(8)
+		line.setOutline(color_rgb(84, 255, 0))
+		line.draw(lWin)
+	
+def evalClick(cX, cY):
+	"""turns player's click into the box's number"""
+	if 0 < cY < 100:
+		if 0 < cX < 100:
+			return 7
+		elif 100 < cX < 200:
+			return 8
+		else:
+			return 9
+	elif 100 < cY < 200:
+		if 0 < cX < 100:
+			return 4
+		elif 100 < cX < 200:
+			return 5
+		else:
+			return 6
+	else:
+		if 0 < cX < 100:
+			return 1
+		elif 100 < cX < 200:
+			return 2
+		else:
+			return 3
+
+def evalCenter(boxNum):
+	"""turns the box's number to its center's coordinates"""
+	if boxNum == 1:
+		return 50, 250
+	elif boxNum == 2:
+		return 150, 250
+	elif boxNum == 3:
+		return 250, 250
+	elif boxNum == 4:
+		return 50, 150
+	elif boxNum == 5:
+		return 150, 150
+	elif boxNum == 6:
+		return 250, 150
+	elif boxNum == 7:
+		return 50, 50
+	elif boxNum == 8:
+		return 150, 50
+	else:
+		return 250, 50
+		
 def checkWin(box11, box12, box13, box21, box22, box23, box31, box32, box33):
 	"""checks to see if there's a winner, returns True if there is, False otherwise"""
 	winner = False
@@ -37,33 +142,38 @@ def checkWin(box11, box12, box13, box21, box22, box23, box31, box32, box33):
 		winner = True
 	return winner
 
-def pTurn(valMove, bVals):
-	print("------------------------- PLAYER TURN ------------------------------")
+def pTurn(valMove, bVals, pWin):
+	"""define player's turn"""
 	# Get the user's move and assert that it's on the board.
-	uMove = input("Please choose the box that you want to go with \n>> ")
-	while not uMove in valMove:
-		uMove = input("Please choose the box that you want to go with \n>> ")
+	pMove = pWin.getMouse()
+	pX = pMove.getX()
+	pY = pMove.getY()
+	pBox = evalClick(pX, pY)
+	while not pBox in valMove:
+		pMove = pWin.getMouse()
+		pX = pMove.getX()
+		pY = pMove.getY()
+		pBox = evalClick(pX, pY)
 	# Change the chosen box's value to "x" if it was the blank before the user's move.
-	bVals[int(uMove) - 1] = "x"
-	valMove.remove(uMove)
-	# Print the board to show the user's move.
-	prBrd(bVals[0], bVals[1], bVals[2], bVals[3], bVals[4], bVals[5], bVals[6], bVals[7], bVals[8])
+	pX, pY = evalCenter(pBox)
+	drawX(pX, pY, pWin)
+	bVals[pBox - 1] = "x"
+	valMove.remove(pBox)
 	# Create a variable to hold the return value of checkWin().
 	wCheck = checkWin(bVals[0], bVals[1], bVals[2], bVals[3], bVals[4], bVals[5], bVals[6], bVals[7], bVals[8])
 	return wCheck
 
-def cTurn(valMove1, bVals1):
-	print("------------------------- COMPUTER TURN ------------------------------")
+def cTurn(valMove1, bVals1, cWin):
+	"""define computer's turn"""
 	# Generate a random move by the computer.
-	cMove = str(randint(1, 9))
-	while not cMove in valMove1:
-		cMove = str(randint(1, 9))
+	cBox = randint(1, 9)
+	while not cBox in valMove1:
+		cBox = randint(1, 9)
+	cX, cY = evalCenter(cBox)
+	drawO(cX, cY, cWin)
 	# Change the chosen box's value to "o" if it was blank before the computer's move.
-	bVals1[int(cMove) - 1] = "o"
-	valMove1.remove(cMove)
-		# Inform the user if the computer couldn't make a valid move.
-	# Print the board to show the computer's move.
-	prBrd(bVals1[0], bVals1[1], bVals1[2], bVals1[3], bVals1[4], bVals1[5], bVals1[6], bVals1[7], bVals1[8])
+	bVals1[cBox - 1] = "o"
+	valMove1.remove(cBox)
 	# Reset the win variable to hold the return value of checkWin() after the computer
 	wiCheck = checkWin(bVals1[0], bVals1[1], bVals1[2], bVals1[3], bVals1[4], bVals1[5], bVals1[6], bVals1[7], bVals1[8])
 	return wiCheck
--- main.py
+++ main.py
@@ -3,27 +3,38 @@
 # import modules
 from funx import *
 from time import *
+from graphics import *
 
 # 9 box variables: row#col#
-boxVals = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+boxVals = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 validMove = [boxVals[0], boxVals[1], boxVals[2], boxVals[3], boxVals[4], boxVals[5], boxVals[6], boxVals[7], boxVals[8]]
 winCheck = False
 
 # The main code starts here.
+ticWin= GraphWin("Tic Tac Toe", 300, 300)
+ticWin.setCoords(0, 0, 300, 300)
 
 # Print the starting, empty board.
-prBrd(boxVals[0], boxVals[1], boxVals[2], boxVals[3], boxVals[4], boxVals[5], boxVals[6], boxVals[7], boxVals[8])
+prBrd(100, 100, ticWin)
 
 # Start a loop that will repeat for more times than the game will need.
 while winCheck == False:
 	# PLAYER TURN
-	winCheck = pTurn(validMove, boxVals)
+	winCheck = pTurn(validMove, boxVals, ticWin)
 	# If the user has won, congratulate the player and exit the program.
 	if winCheck == True:
-		print("You win! Congratulation!")
+		drawLine(boxVals[0], boxVals[1], boxVals[2], boxVals[3], boxVals[4], boxVals[5], boxVals[6], boxVals[7], boxVals[8], ticWin)
+		print("You win! Congratulations!")
+	elif validMove == []:
+		print("Game tie! Try again!")
+		winCheck = True
 	else:
 		# COMPUTER TURN
-		sleep(1.5)
-		winCheck = cTurn(validMove, boxVals)
+		sleep(1)
+		winCheck = cTurn(validMove, boxVals, ticWin)
 		if winCheck == True:
+			drawLine(boxVals[0], boxVals[1], boxVals[2], boxVals[3], boxVals[4], boxVals[5], boxVals[6], boxVals[7], boxVals[8], ticWin)
 			print("You lose! Sorry")
+		
+ticWin.getMouse()
+ticWin.close()
